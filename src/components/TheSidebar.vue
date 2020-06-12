1f706304de2cf1999470bc24f74be8a914fcd17e
erbsca <atgmaledatcom@gmail.com>
2020-06-12 20:25:29 +0800

    refactor

---
+++ src/components/TheSidebar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="sidebar">
+        <TheLogo />
+        <TheNavigation />
+    </div>
+</template>
+
+<script>
+import TheLogo from "./TheLogo.vue";
+import TheNavigation from "./TheNavigation.vue";
+export default {
+    name: "TheSidebar",
+    components: {
+        TheLogo,
+        TheNavigation
+    }
+};
+</script>
+
+<style>
+.sidebar {
+    color: #ffffff;
+    background-color: #43425d;
+    width: 15vw;
+    min-width: 160px;
+}
+@media (max-width: 768px) {
+    .sidebar {
+        width: 10vw;
+        min-width: 10vw;
+    }
+}
+</style>
